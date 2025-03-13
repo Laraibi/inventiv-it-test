cfc00941e4669fdab77cfb843c8f48a0f21d1fbf
Laraibi Mehdi <meh.laraibi@gmail.com>
2025-03-13 01:41:44 +0000

    redirect root to /calculator

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalculatorController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/calculator');
 });
 
 
